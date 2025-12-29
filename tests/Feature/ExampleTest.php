937fae70ffdc7cf5089d369d4253b4f0b7f8685f
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-12-29 17:04:39 +0200

    chore: run tests in CI

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,13 @@
 <?php
 
+use Inertia\Testing\AssertableInertia;
+
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
+    $response->assertInertia(fn (AssertableInertia $page) =>
+        $page
+            ->component('Todo/List')
+    );
     $response->assertStatus(200);
 });
